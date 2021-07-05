2c2052d3819a6352111ae865710337b1f4d40177
Radoslaw Domanski <woohaas@gmail.com>
2021-07-05 21:07:07 +1200

    after section 13 - options

--- exercises/option/option1.rs
+++ exercises/option/option1.rs
@@ -1,23 +1,30 @@
 // option1.rs
 // Make me compile! Execute `rustlings hint option1` for hints
 
-// I AM NOT DONE
+use std::fmt;
 
 // you can modify anything EXCEPT for this function's sig
 fn print_number(maybe_number: Option<u16>) {
-    println!("printing: {}", maybe_number.unwrap());
+    match maybe_number {
+        None => println!("None"),
+        Some(arg) => println!("Some: {}", arg)
+    };
 }
 
 fn main() {
-    print_number(13);
-    print_number(99);
+    print_number(Some(13));
+    print_number(Some(99));
 
-    let mut numbers: [Option<u16>; 5];
+    let mut numbers: [Option<u16>; 6] = [None;6];
     for iter in 0..5 {
         let number_to_add: u16 = {
             ((iter * 1235) + 2) / (4 * 16)
         };
 
-        numbers[iter as usize] = number_to_add;
+        numbers[iter as usize] = Some(number_to_add);
+    }
+
+    for (i, &x) in numbers.iter().enumerate() {
+        print_number(x);
     }
 }
